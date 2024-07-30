002ca5e34526a37333cb9e57ea382439c0343bff
Matt Hartley <matt.hartley@gmail.com>
2024-07-30 12:14:26 -0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -14,5 +14,6 @@ sudo mkdir -p /etc/tuned/intel-best_power_efficiency_mode
 sudo cp profiles/intel-best_performance_mode/tuned.conf /etc/tuned/intel-best_performance_mode/tuned.conf
 sudo cp profiles/intel-best_power_efficiency_mode/tuned.conf /etc/tuned/intel-best_power_efficiency_mode/tuned.conf
 
-# Enable and start the tuned service 
+# Reinstall (for Intel settings to show up) enable and start the tuned service 
+sudo reinstall tuned -y
 sudo systemctl enable --now tuned
