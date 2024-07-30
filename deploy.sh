94ecd66cb89fb2823b17c34cd54154004d3ad271
Matt Hartley <matt.hartley@gmail.com>
2024-07-30 11:04:07 -0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -14,8 +14,8 @@ sudo mkdir -p /etc/tuned/intel-best_power_efficiency_mode
 sudo cp profiles/intel-best_performance_mode/tuned.conf /etc/tuned/intel-best_performance_mode/tuned.conf
 sudo cp profiles/intel-best_power_efficiency_mode/tuned.conf /etc/tuned/intel-best_power_efficiency_mode/tuned.conf
 
-# Reinstall and restart Tuned to catch the profiles
-# Found without both reinstall and restart, it will not activate correctly 
-sudo dnf reinstall tuned -y && sudo systemctl restart tuned
+# Reinstall and restart Tuned to catch the profiles found without both reinstall and restart, it will not activate correctly 
+sudo dnf reinstall tuned -y 
+sudo systemctl restart tuned
 
 echo "Profiles deployed."
