2e2c0029eca8ab5f65a411c6ce55c0daae6e63e0
Matt Hartley <matt.hartley@gmail.com>
2024-07-30 06:22:43 -0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,9 @@
 # Deploy profiles
 echo "Deploying profiles..."
 
+# First time installing Tuned
+sudo dnf install tuned -y
+
 # Create directory for custom profiles if it doesn't exist
 sudo mkdir -p /etc/tuned/intel-best_performance_mode
 sudo mkdir -p /etc/tuned/intel-best_power_efficiency_mode
@@ -11,8 +14,8 @@ sudo mkdir -p /etc/tuned/intel-best_power_efficiency_mode
 sudo cp profiles/intel-best_performance_mode/tuned.conf /etc/tuned/intel-best_performance_mode/tuned.conf
 sudo cp profiles/intel-best_power_efficiency_mode/tuned.conf /etc/tuned/intel-best_power_efficiency_mode/tuned.conf
 
-# Restart the tuned service
-sudo systemctl restart tuned
+# Reinstall Tuned to catch the profiles
+sudo dnf reinstall tuned -y
 
 # Activate a specific profile (choose one to activate initially, e.g., best performance mode)
 sudo tuned-adm profile intel-best_performance_mode
